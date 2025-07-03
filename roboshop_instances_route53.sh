1c068267d0fc2bff573887a062cb12a5ce96f55e
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 14:43:01 +0530

    creating instances and editing route 53 records using AWS CLI and shell script for roboshop website

--- roboshop_instances_route53.sh
+++ roboshop_instances_route53.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0176514X7F0V5F420IF" # aws route53 cone ID
 DOMAIN_NAME="rahuldaws.shop" # roboshop website DNS
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
