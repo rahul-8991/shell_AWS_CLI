9de344e7cd2b857dd6fc3d228305b590baf0307a
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 16:25:17 +0530

    get public ips of all my AWS servers

--- get_ips.sh
+++ get_ips.sh
@@ -7,17 +7,9 @@ INSTANCES=($(aws ec2 describe-instances \
 
 for instance in "${INSTANCES[@]}"
 do
-    if [ "$instance" != "frontend" ]; then
-        IP=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$instance" \
-            --query "Reservations[0].Instances[0].PrivateIpAddress" \
-            --output text)
-    else
         IP=$(aws ec2 describe-instances \
             --filters "Name=tag:Name,Values=$instance" \
             --query "Reservations[0].Instances[0].PublicIpAddress" \
             --output text)
-    fi
-
     echo "$instance IP address: $IP"
 done
