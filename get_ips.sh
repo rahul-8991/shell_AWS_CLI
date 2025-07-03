ebb78c1b39fade9683cd4f9406becbeb5d2c1419
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 15:53:32 +0530

    get ips of servers

--- get_ips.sh
+++ get_ips.sh
@@ -7,7 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 for instance in ${INSTANCES[@]}
 #for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 describe-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0c353a79d89785c5b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 describe-instances  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
