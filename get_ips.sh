25d801c051182979a9418824e5f6ee882a983881
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 16:17:09 +0530

    get private ips of all AWS servers except public ip for frontend

--- get_ips.sh
+++ get_ips.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Dynamically fetch all instance names (tag:Name) for running instances
+# Get all instance names from all instances in any state
 INSTANCES=($(aws ec2 describe-instances \
-  --filters "Name=instance-state-name,Values=running" \
   --query "Reservations[*].Instances[*].Tags[?Key=='Name'].Value[]" \
   --output text))
 
@@ -10,14 +9,15 @@ for instance in "${INSTANCES[@]}"
 do
     if [ "$instance" != "frontend" ]; then
         IP=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$instance" "Name=instance-state-name,Values=running" \
+            --filters "Name=tag:Name,Values=$instance" \
             --query "Reservations[0].Instances[0].PrivateIpAddress" \
             --output text)
     else
         IP=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$instance" "Name=instance-state-name,Values=running" \
+            --filters "Name=tag:Name,Values=$instance" \
             --query "Reservations[0].Instances[0].PublicIpAddress" \
             --output text)
     fi
+
     echo "$instance IP address: $IP"
 done
