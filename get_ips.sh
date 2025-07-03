5dccc58dc335b3d701d93cc305ec70c399fbc9f9
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 15:46:10 +0530

    get ips of servers

---
+++ get_ips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0c353a79d89785c5b" # Allow all traffic SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+
+for instance in ${INSTANCES[@]}
+#for instance in $@
+do
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address: $IP"
+done
