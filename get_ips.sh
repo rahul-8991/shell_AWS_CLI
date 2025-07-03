8d0de7aa73a40f2e5a3fb38c2914f61caaa99a8f
unknown <kudikyalarahul8055@gmail.com>
2025-07-03 16:11:09 +0530

    get ips of servers

--- get_ips.sh
+++ get_ips.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for instance in ${INSTANCES[@]}
-#for instance in $@
+for instance in "${INSTANCES[@]}"
 do
-    INSTANCE_ID=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].InstanceId" --output text)
-    if [ $instance != "frontend" ]
-    then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --filters "Name=tag:Name,Values=$instance" "Name=instance-state-name,Values=running" \
+            --query "Reservations[0].Instances[0].PrivateIpAddress" \
+            --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances \
+            --filters "Name=tag:Name,Values=$instance" "Name=instance-state-name,Values=running" \
+            --query "Reservations[0].Instances[0].PublicIpAddress" \
+            --output text)
     fi
     echo "$instance IP address: $IP"
 done
