4afb77e1ee8e2702aa3a4b13e5ccd07387abeffe
unknown <kudikyalarahul8055@gmail.com>
2025-07-07 12:26:09 +0530

    redis script file

--- redis.sh
+++ redis.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+#This will give current timestamp in seconds, by default it calculates since 1970
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -39,13 +41,14 @@ VALIDATE $? "Disabling Redis"
 dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enabling Redis"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
 #-i edits the file in place and permanently
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "Editing Redis conf file for remote connections"
 
+#or we can give both sed in  one line as : sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 #^: Anchors the match to the start of the line; protected-mode: The exact key to match; [[:space:]]\+: Matches one or more spaces or tabs; yes: The value you're changing; protected-mode no: The replacement string
 sed -i 's/^protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "Editing Redis conf file protected mode"
@@ -57,3 +60,7 @@ systemctl start redis
 VALIDATE $? "starting Redis service"
 
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
